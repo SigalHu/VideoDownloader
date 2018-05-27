b307471cbbfd6af0368c364f95bb3c7a0bfcffbe
huxujun <811734597@qq.com>
2018-05-27 15:20:00 +0800

    增加断点续传

--- VideoDownloader/pipelines.py
+++ VideoDownloader/pipelines.py
@@ -14,6 +14,13 @@ from VideoDownloader.spiders.settings import SAVE_PATH
 
 
 class VideoDownloaderPipeline(object):
+    download_header = {
+        "Accept-Encoding": "identity;q=1, *;q=0",
+        "Range": None,
+        "Referer": None,
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.59 Safari/537.36 115Browser/8.6.2"
+    }
+
     def process_item(self, item, spider):
         file_path = item["title"] + "." + item["type"]
         try:
@@ -35,10 +42,25 @@ class VideoDownloaderPipeline(object):
         path = os.path.dirname(file_name)
         if not os.path.exists(path):
             os.mkdir(path)
-        resp = requests.get(video_url, stream=True)
-        with open(file_name, 'wb') as file:
-            file.write(resp.content)
-            file.flush()
+
+        content_offset = 0
+        content_length = 1024 * 1024 * 10
+        total_length = None
+        self.download_header["Referer"] = video_url
+        while True:
+            self.download_header["Range"] = "bytes=%d-%d" % (content_offset, content_offset + content_length)
+            resp = requests.get(video_url, stream=True, headers=self.download_header)
+            resp_length = int(resp.headers["Content-Length"])
+            if total_length is None:
+                total_length = int(resp.headers["Content-Range"].split("/")[1])
+
+            with open(file_name, 'ab') as file:
+                file.write(resp.content)
+                file.flush()
+
+            content_offset += resp_length
+            if content_offset >= total_length:
+                break
 
     def get_valid_name(self, expected_file_name, instead_str=' '):
         valid_name = re.compile(r'[/\\:*?"<>|]').sub(instead_str, expected_file_name)
